e15e672d38d4d16d6670fa6f224266b49e54f968
胡双飞 <2868352478@qq.com>
2017-09-30 15:34:49 +0800

    摄像机跟随

---
+++ ActionGameProject/Assets/Script/FolowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolowPlayer : MonoBehaviour {
+
+
+	public float speed = 2;
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+	   player = GameObject.FindGameObjectWithTag(Tags.playerTag).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//摄像机跟随位置
+		Vector3 targetPos = player.position + new Vector3(0.5f,5.4f,-16.29f);
+		transform.position = Vector3.Lerp(transform.position,targetPos,speed * Time.deltaTime);
+
+		//摄像机跟随角度
+		Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,speed * Time.deltaTime);
+	}
+}
---
+++ ActionGameProject/Assets/Script/Tags.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags  {
+
+	public const string playerTag = "Player";
+   
+}
